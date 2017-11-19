e66ca62614b6e56c1882297969ec246fb905e28d
jose <jose.cornado@efekctive.con>
2017-11-19 09:33:36 -0700

    init commit

---
+++ launch.sh
@@ -0,0 +1,14 @@
+export JAVA_HOME=/usr/java/default
+sudo service mongod start
+cd source
+cd vaadin-frontend
+git pull
+mvn clean install
+#avoid taxing memory
+mvn spring-boot:run&
+cd ../spring-backend
+git pull
+mvn clean install spring-boot:run&
+#mvn spring-boot:run -Drun.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
+#jdb -attach 5005
+cd ..
