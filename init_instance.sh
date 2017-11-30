22b3c2f57d80be22892b64dd58c91e5cca5382b1
jose <josedanconstruction@gmail.com>
2017-11-30 10:15:15 -0700

    lastest version

--- init_instance.sh
+++ init_instance.sh
@@ -10,7 +10,7 @@ sudo mv ./mongodb-org-3.0.repo /etc/yum.repos.d/mongodb-org-3.0.repo
 sudo yum update
 # sudo iptables -A INPUT -i eth0 -p tcp --dport 80 -j ACCEPT
 #sudo iptables -A INPUT -i eth0 -p tcp --dport 8080 -j ACCEPT
-#sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
+sudo iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
 #sudo iptables -A INPUT -p tcp -s 127.0.0.1 --dport 8081 -j ACCEPT
 #sudo iptables -A INPUT -p tcp --dport 8081 -j DROP
 #sudo service iptables save
--- launch.sh
+++ launch.sh
@@ -8,7 +8,8 @@ mvn clean install
 mvn spring-boot:run&
 cd ../spring-backend
 git pull
-mvn clean install spring-boot:run&
+mvn clean install
+mvn spring-boot:run&
 #mvn spring-boot:run -Drun.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
 #jdb -attach 5005
 cd ..
